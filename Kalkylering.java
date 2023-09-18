267091a3da0038cf67d9ffdfd976eee248651625
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2023-09-18 09:25:09 +0200

    Klar med uppgift A

---
+++ Kalkylering.java
@@ -0,0 +1,14 @@
+public class Kalkylering{
+    public static void main(String[] args){
+        System.out.println("Uppg A");
+        int tal1 = 23;
+        int tal2 = 45;
+        int summa_add;
+        int summa_sub;
+        summa_add = tal1+tal2;
+        summa_sub = tal1-tal2;
+
+        System.out.println(tal1+"+"+tal2+"="+summa_add);
+        System.out.println(tal1+"-"+tal2+"="+summa_sub);
+    }
+}
